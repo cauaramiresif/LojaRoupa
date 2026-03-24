ce7d9fc9c0ad14e06f8c0fc0a82a4052fd2a410c
cauaramiresif <cauaramires.bg003@academico.ifsul.edu.br>
2026-03-23 22:27:20 -0300

    Vale nota

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/camisas', (req, res) => {
     const html = `
     <div>
         <a href="/camisas">Camisas</a>
-        <p>Polo Branca: R$ 30,00. <a href="">Ver mais</a></p>
-        <p>Regata do Chaves: R$ 100,00. <a href="">Ver mais </a></p>
+        <p>Polo Branca: R$ 30,00. <a href="/camisas/polobranca">Ver mais</a></p>
+        <p>Regata do Chaves: R$ 100,00. <a href="/camisas/regatadochaves">Ver mais </a></p>
         <a href="/bermudas">Bermudas</a>
         <a href="/meias">Meias</a>
     </div>
@@ -27,10 +27,23 @@ app.get('/camisas', (req, res) => {
 })
 
 
-app.get('/', (req, res) => {
+app.get('/camisas/polobranca', (req, res) => {
     const html = `
     <div>
-        
+        <p>Polo Branca</p>
+        <p>R$30,00</p>
+        <p>Tamanhos: P, M, G, GG</p>
+    </div>
+    `
+    res.send(html)
+})
+
+app.get('/camisas/regatadochaves', (req, res) => {
+    const html = `
+    <div>
+        <p>Regata do Chaves</p>
+        <p>R$100,00</p>
+        <p>Tamanhos: P, M, G, GG</p>
     </div>
     `
     res.send(html)
